741caf76225a17cd316df8cf2e2e3fff8b64ded9
MauricioRTG <44274231+MauricioRTG@users.noreply.github.com>
2024-03-09 12:48:53 -0600

    Add new classes based on the UML diagram
    -TimerWidget
    -PickupItem
    -SprayPaintCan
    -GameOverWidget
    -PointsWidget

---
+++ Source/SkatePark/SprayPaintCan.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PickupItem.h"
+#include "SprayPaintCan.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKATEPARK_API ASprayPaintCan : public APickupItem
+{
+	GENERATED_BODY()
+	
+};
